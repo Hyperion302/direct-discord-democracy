a7145b0eafb5c38c53c2e524e18b5e6556d1aa0a
Joseph Shetaye <josephs@Josephs-MacBook-Pro.local>
2018-07-21 17:37:46 -0500

    Finalized errors.

--- errors.py
+++ errors.py
@@ -1,15 +1,15 @@
 # Replace error messages with something that makes sense
 class UserError(Exception):
-	def __init__(self, input_segment):
+	def __init__(self, err_input):
 		super().__init__("There was an error with your command")
-		self.input_segment = input_segment
+		self.err_input = err_input
 
 class DatabaseError(Exception):
-	def __init__(self, error):
+	def __init__(self, query):
 		super().__init__("The database could not be reached")
-		self.error = error
+		self.query = query
 
 class SoftwareError(Exception):
-	def __init__(self, userQuery):
+	def __init__(self, error):
 		super().__init__("An unexpected error occured in the backend")
-		self.userQuery = userQuery
+		self.error = error
