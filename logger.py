fae219a9b8385ca5147f8ace4d762bc16ae3e012
Joseph Shetaye <josephs@Josephs-MacBook-Pro.local>
2018-07-23 15:06:16 -0500

    Made all methods that call send_message async and made all calls to send_message await; Colors are passed to discord.Embed as color objects, not classmethods; Added stdout prints to go along with server readouts.

--- logger.py
+++ logger.py
@@ -1,33 +1,37 @@
 import discord
+import asyncio
 
 class Logger:
     """Logs messages to channels and provides debug logging"""
     def __init__(self,client):
         self.client = client
         self.colors = {
-            "error": discord.Color.red,
-            "log": discord.Color.blue,
-            "success": discord.Color.green,
-            "status": discord.Color.dark_green
+            "error": discord.Color.red(),
+            "log": discord.Color.blue(),
+            "success": discord.Color.green(),
+            "status": discord.Color.dark_green()
         }
     
-    def error(self,error_message,channel):
+    async def error(self,error_message,channel):
         """Logs an error message in a channel"""
-        embed = discord.Embed(type="rich",colour=self.colors['error'],description=error_message)
-        self.client.send_message(channel,embed)
+        embed = discord.Embed(type="rich",color=self.colors['error'],description=error_message)
+        print("[ERROR] %s" % error_message)
+        await self.client.send_message(channel,embed = embed)
     
-    def log(self,log_message,channel):
+    async def log(self,log_message,channel):
         """Logs a neutral message in a channel"""
-        embed = discord.Embed(type="rich",colour=self.colors['log'],description=log_message)
-        self.client.send_message(channel,embed)
+        embed = discord.Embed(type="rich",color=self.colors['log'],description=log_message)
+        print("[LOG] %s" % log_message)
+        await self.client.send_message(channel,embed = embed)
     
-    def success(self,success_message,channel):
+    async def success(self,success_message,channel):
         """Logs a success message in a channel"""
-        embed = discord.Embed(type="rich",colour=self.colors['success'],description=success_message)
-        self.client.send_message(channel,embed)
+        embed = discord.Embed(type="rich",color=self.colors['success'],description=success_message)
+        print("[SUCCESS] %s" % success_message)
+        await self.client.send_message(channel,embed = embed)
 
-    def status(self,action,channel):
+    async def status(self,action,channel):
         """Logs a status message and returns it's ID"""
         log_message = '\n'.join([element for element in action.serialize()])
-        embed = discord.Embed(type="rich",colour=self.colors['status'],description=log_message)
-        self.client.send_message(channel,embed)
+        embed = discord.Embed(type="rich",color=self.colors['status'],description=log_message)
+        await self.client.send_message(channel,embed = embed)
