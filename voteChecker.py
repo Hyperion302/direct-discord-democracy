ebec1fe942ad512dd25c6f3316821882f8b5c2c0
Joseph Shetaye <Hyperion302@gmail.com>
2018-07-30 16:19:27 -0500

    Reorganized voteChecker to be more readable.

--- voteChecker.py
+++ voteChecker.py
@@ -14,28 +14,45 @@ class voteCheckingClient(discord.Client):
             for action in self.table.find({'active':True}):
                 channel = self.get_channel(action['channelId'])
                 if not channel:
-                    print("Missed channel")
+                    print("Missed channel") #TODO: Error handling
                     continue
+
                 server = channel.server
                 action = Action.DDDAction(action)
+
+                print("Checking action #%d" % action.internalID)
+
                 # Check delay
                 serverData = await self.sw.getServerData(server)
                 delay = serverData['delay']
-                if action.created_at+delay <= int(datetime.datetime.utcnow().timestamp()):
-                    # Tally votes and check ratio
-                    yae = action.y
-                    nae = action.n
-                    # Check quorum
-                    if (yae+nae)/server.member_count < serverData['quorum']:
-                        print("Quorum not reached")
-                        continue
-                    ratio = yae/(yae+nae) #TODO: Should I be doing this?
-                    if ratio <= action.threshold:
-                        print("Threshold not reached")
-                    # If the vote passed, call action
-                    print("Executing action #%d" % action.internalID)
-                    await action.execute(self,server,self.logger)
-                    self.table.update_one({"messageId":action.messageId},{"$set":{"active":False}})
+                if action.created_at+delay > int(datetime.datetime.utcnow().timestamp()):
+                    print("Delay not reached")
+                    continue
+
+                # Tally votes and check ratio
+                yae = action.y
+                nae = action.n
+                    
+                # Check quorum
+                totalPercent = (yae+nae)/server.member_count
+                if totalPercent < serverData['quorum']:
+                    print("Quorum not reached")
+                    continue
+                    
+                # Check threshold
+                ratio = yae/(yae+nae)
+                if ratio <= action.threshold:
+                    print("Threshold not reached")
+                    continue
+                    
+                # If the vote passed, call action
+                print("Executing action #%d" % action.internalID)
+                await self.logger.success("Executing action #%d" % action.internalID,channel)
+                await action.execute(self,server,self.logger)
+                
+                # Deactivate prop
+                self.table.update_one({"messageId":action.messageId},{"$set":{"active":False}})
+                print("\n")
             executionTime = (asyncio.get_event_loop().time())-startTime
             print("Done checking votes after %d seconds" % executionTime)
             await asyncio.sleep(60-executionTime)
